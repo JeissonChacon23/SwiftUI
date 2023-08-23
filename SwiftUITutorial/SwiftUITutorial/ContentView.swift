73ce10a009ecd0f84baddf2a74ffa3a29094ae18
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-23 09:20:35 -0500

    Text("") .padding()

--- SwiftUITutorial/SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/SwiftUITutorial/ContentView.swift
@@ -5,27 +5,40 @@
 //  Created by Jeisson Chacón on 8/23/23.
 //
 
-import SwiftUI //Tenemos que importar la clase SwiftUI
-struct ContentView: View{ //ContentView es el nombre de la vista
-    var body: some View{ //Una variable llamada body que tiene que devolver algo de tipo View
+
+
+
+
+
+
+import SwiftUI
+struct ContentView: View{
+    var body: some View{
         VStack{
-            Image(systemName: "globe")//invoca una imagen del sistema
-                .foregroundColor(.blue)
-                .imageScale(.large)//Aumenta el tamanio
-            Text("Hello, World!")
-                .foregroundColor(.blue)//Cambiar el color
-                .font(.title2)
+            Text("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+            Text("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+            Text("ccccccccccccccccccccccccccccccccccccccccccc")
+            
+            Text("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+                .padding() //Deja unos margenes a los lados 
+            Text("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+                .padding()
+            Text("cccccccccccccccccccccccccccccccccccccc")
+                .padding()
         }
-        .padding()
     }
 }
 
-//Esto permite que se muestre el canvas 
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
-//Puedo usar Command + Click para mostar las opciones de una perte del codigo que este seleccionado
