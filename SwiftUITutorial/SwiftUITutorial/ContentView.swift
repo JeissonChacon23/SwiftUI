8c0aac318ea020cfffbde94b2cf8ae2dfe1bf2fd
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-23 12:29:47 -0500

    HStack

--- SwiftUITutorial/SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/SwiftUITutorial/ContentView.swift
@@ -16,32 +16,6 @@ struct ContentView: View{
     var body: some View{
         VStack(){
             HStack{
-                Spacer()
-                Text("888888888888888888888888888888888888888")
-                Text("666666666666666666666666666666666666666")
-            }
-                Text("8888888888888888888888888888888888888")
-                Spacer()
-            HStack{
-                Text("8888888888888888")
-                    .multilineTextAlignment(.leading)
-                    .background(.cyan)
-                Text("8888888888888888")
-                    .background(.bar)
-            }
-            .background(.purple)
-            
-            
-            Spacer() //Empuja los elementos para que ocupen lo maximo posible
-            
-            
-            Text("00000000000000000000000000000000000")
-                .padding()
-                .background(.red)
-            
-            Spacer()
-            
-            HStack{
                 Text("00000000000000000000000000000000000")
                     .padding()
                     .background(.green)
@@ -49,8 +23,13 @@ struct ContentView: View{
                     .padding()
                     .background(.yellow)
             }
+            Text("00000000000000000000000000000000000")
+                .padding()
+                .background(.green)
+            Text("00000000000000000000000000000000000")
+                .padding()
+                .background(.yellow)
         }
-        .background(.gray)
     }
 }
 
